af0d4eed2727928a8ee05069c64140dceb3e5087
olgKuz <kuznecovao@list.ru>
2025-06-23 20:19:56 +0300

    23/06/25

--- src/app/layout/header/header.component.ts
+++ src/app/layout/header/header.component.ts
@@ -1,17 +1,19 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit, HostListener } from '@angular/core';
 import { ButtonModule } from 'primeng/button';
 import { MenubarModule } from 'primeng/menubar';
 import { MenuItem } from 'primeng/api';
 import { IUser } from '../../models/user';
 import { UserService } from '../../services/user.service';
 import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-header',
   standalone: true,
   imports: [
     MenubarModule,
-    ButtonModule
+    ButtonModule, 
+    CommonModule
   ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
@@ -21,6 +23,8 @@ export class HeaderComponent implements OnInit, OnDestroy {
   menuItems: MenuItem[] = [];
   user: IUser;
   logoutIcon = 'pi pi-user';
+  isMobile = false;
+  menuVisible = false;
 
   constructor(
     private userService: UserService,
@@ -30,32 +34,35 @@ export class HeaderComponent implements OnInit, OnDestroy {
   ngOnInit(): void {
     this.user = this.userService.getUser();
     this.menuItems = this.initMenuItems();
+    this.checkScreenSize();
   }
 
   ngOnDestroy() {}
 
   initMenuItems(): MenuItem[] {
     return [
-      {
-        label: 'Главная',
-        routerLink: ['/home']
-      },
-      {
-        label: 'Галерея',
-        routerLink: ['/gallery']
-      },
-      {
-        label: 'Информация',
-        routerLink: ['/blog']
-      },
-      {
-        label: 'Дизайнерам',
-        routerLink: ['/desauth']
-      }
+      { label: 'Главная', routerLink: ['/home'] },
+      { label: 'Галерея', routerLink: ['/gallery'] },
+      { label: 'Информация', routerLink: ['/blog'] },
+      { label: 'Дизайнерам', routerLink: ['/desauth'] }
     ];
   }
 
   goToContacts(): void {
     this.router.navigate(['/contacts']);
+  }
+
+  navigate(route: any[]): void {
+    this.router.navigate(route);
+    this.menuVisible = false;
+  }
+
+  @HostListener('window:resize', [])
+  onResize() {
+    this.checkScreenSize();
+  }
+
+  private checkScreenSize(): void {
+    this.isMobile = window.innerWidth < 768;
   }
 }
