141a2101d42783114df2a6b1f574aae1510a581c
olgKuz <kuznecovao@list.ru>
2025-09-19 19:41:46 +0300

    19.09.25

--- src/app/pages/contacts/contacts.component.ts
+++ src/app/pages/contacts/contacts.component.ts
@@ -18,10 +18,10 @@ import { ContactformComponent } from '../home/contactform/contactform.component'
   styleUrls: ['./contacts.component.scss'],
 })
 export class ContactsComponent {
-  phone = '+7 (123) 456-78-90';
-  phoneTel = '+71234567890'; // для tel:
-  email = 'info@designstudio.ru';
+  phone = '+7 (911) 029-30-30';
+  phoneTel = '+79110293030'; // для tel:
+  email = 'mail@customglass.ru';
   // можно Google Maps, можно Яндекс. Ниже пример для Яндекс.Карт по адресу из макета:
-  mapUrl = 'https://yandex.ru/maps/?mode=search&text=Москва,%20ул.%20Дизайнеров,%2015';
+  mapUrl = 'https://yandex.ru/maps/?mode=search&text=Санкт-Петербург,%20ул.%20Железноводская,%203';
   public encodeURIComponent = encodeURIComponent;
 }
