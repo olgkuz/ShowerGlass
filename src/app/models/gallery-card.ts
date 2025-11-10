74fc7887787674342d352b8dfd4a37d7fb4a3552
olgKuz <kuznecovao@list.ru>
2025-11-10 18:31:28 +0300

    10.11.25

--- src/app/models/gallery-card.ts
+++ src/app/models/gallery-card.ts
@@ -3,5 +3,5 @@ export interface IGalleryCard {
   name: string;
   description: string;
   img?: string;       // имя файла, сохранённое на сервере (например, card-12345.jpg)
-  imgUrl?: string;    // абсолютная ссылка на изображение (например, http://localhost:3000/public/card-12345.jpg)
+  imgUrl?: string;    // абсолютная ссылка на изображение (например, https://dushcabs.onrender.com/public/card-12345.jpg)
 }
